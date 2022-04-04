1d0037f877296dbbe46600d2e9772bf17c5ed2e7
岳泽以 <97122019+Yue-Zeyi@users.noreply.github.com>
2022-04-04 09:18:10 +0800

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,9 +1,8 @@
 <?php if (!defined('__TYPECHO_ROOT_DIR__')) exit; ?>
 <footer  class="container"  role="contentinfo">
-    <p class="footerb">&copy; <?php echo date('Y'); ?>  <a href="<?php $this->options->siteUrl(); ?>"><?php $this->options->title() ?></a> Theme By <a href="https://www.krsay.com/typecho/fantasy.html">Ltns</a></p>
+    <p class="footerb">&copy; <?php echo date('Y'); ?>  <a href="<?php $this->options->siteUrl(); ?>"><?php $this->options->title() ?></a> Theme By <a href="https://www.yuezeyi.com/ltns.html" target="_blank">Ltns</a></p>
 	<p class="footerb"><a href="http://beian.miit.gov.cn" class="icp" target="_blank" rel="noreferrer"><?php $this->options->ICPbeian(); ?></a></p>
 	<?php $this->footer(); ?>
 <?php if ($this->options->CustomContent): $this->options->CustomContent(); ?>
 <?php endif; ?>
-
 </footer>
